faac60556dbbc012d7c7e01b752a68010f59b78e
tuxisawesome <67063826+tuxisawesome@users.noreply.github.com>
2023-05-09 17:50:05 -0400

    Update mine.sh

--- mine.sh
+++ mine.sh
@@ -1,5 +1,23 @@
 #!/bin/bash
 sudo apt update
+sudo apt install dialog
+
+# dynbox.sh - Yes/No box demo
+dialog --title "Install Miner" \
+--backtitle "Walter's Utilities" \
+--yesno "Are you sure you want to install the miner?" 7 60
+
+# Get exit status
+# 0 means user hit [yes] button.
+# 1 means user hit [no] button.
+# 255 means user hit [Esc] key.
+response=$?
+case $response in
+   0) echo "Installing miner...";;
+   1) exit;;
+   255) exit;;
+esac
+
 sudo apt dist-upgrade -y
 sudo apt install git build-essential cmake libuv1-dev libssl-dev libhwloc-dev -y
 git clone https://github.com/xmrig/xmrig.git
